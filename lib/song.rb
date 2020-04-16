abaae222c6d932cc8f330ac9fc3126b87a36a3a8
Sonja Parsell <hello@sparsell.com>
2020-04-16 14:04:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,8 @@ class Song
 @@count = 0
 @@artists = []
 @@genres = []
-@@genre_count = {}
-@@artist_count = {}
+# @@genre_count = {}
+# @@artist_count = {}
 
   def initialize(name, artist, genre)
     @name = name
