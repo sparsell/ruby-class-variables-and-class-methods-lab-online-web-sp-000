7df76f61f3bf5dc37386d6042385e9ec08869330
Sonja Parsell <hello@sparsell.com>
2020-04-16 15:13:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,15 @@ class Song
   end
 
   def self.artist_count
-    @@artist_count = {}
+    artist_count = {}
+    @@artist.each do |artist|
+      if artist_count[artist]
+          artist_count[artist] +=1
+        else
+          artist_count[artist] = 1
+        end
+      end
+      artist_count
   end
 
 end
